2139eeb8e049362cae5dc8fedc0fbfa46d273f1b
Imerua123 <santon2228@gmail.com>
2026-04-17 20:32:28 +0300

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -409,6 +409,27 @@ export default {
             content: fullText,
             ephemeral: true // 👈 только тому, кто нажал
         });
+      if (interaction.isButton()) {
+    if (interaction.customId === 'full_schedule') {
+
+        const schedule = {
+            Monday: "Понедельник:\n- Тренировка\n- Работа\n- Учеба",
+            Tuesday: "Вторник:\n- Кодинг\n- Спорт",
+            Wednesday: "Среда:\n- Отдых\n- Игры",
+            Thursday: "Четверг:\n- Учеба\n- Проект",
+            Friday: "Пятница:\n- Работа\n- Вечеринка",
+            Saturday: "Суббота:\n- Отдых\n- Друзья",
+            Sunday: "Воскресенье:\n- Чилл\n- Подготовка к неделе"
+        };
+
+        const fullText = Object.values(schedule).join("\n\n");
+
+        await interaction.reply({
+            content: fullText,
+            ephemeral: true
+        });
+    }
+}
     }
 }
         }
